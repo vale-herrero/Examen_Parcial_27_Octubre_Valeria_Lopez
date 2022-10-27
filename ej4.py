fb4c293593d6fa5b286b5f5283b937cbd0415810
vale-herrero <vale4134@gmail.com>
2022-10-27 19:43:37 +0200

    hecho la mitad de ej4

--- ej4.py
+++ ej4.py
@@ -1,1 +1,18 @@
 
+class Alumno():
+    def __init__(self, nombre, nota):
+        self.nombre= nombre
+        self.nota= nota
+        print('se ha creado el alumno con exito')
+    
+    def calificacion(self, nota):
+        if nota>4 and nota<10:
+            print('El alumno ha aprobado')
+        elif nota<=4:
+            print('El alumno ha suspenso')
+        else:
+            print('El alumno no tiene una nota valida')
+    def __str__(self): return 'lo que quiero mostrar'
+
+alumno1= Alumno('Valeria', 8)
+print(alumno1)
