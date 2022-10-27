13b92ab0fcb9b06ca41e2c967da3c6f2f08fca98
vale-herrero <vale4134@gmail.com>
2022-10-27 20:46:58 +0200

    update al ej 6

--- ej6.py
+++ ej6.py
@@ -8,3 +8,12 @@ def determinante(matriz):
 
 matriz = np.array([[3,5,6,7,8],[1,2,3,4,5],[3,6,8,3,9],[8,3,8,5,1],[7,6,5,4,3]])
 print(determinante(matriz))
+
+def det(matriz):
+    det=1
+    for x in range(matriz):
+        det= matriz[x][x]*det
+    print('el determinante de la matriz es: ', det)
+
+matriz1= [[3,5,6,7,8],[1,2,3,4,5],[3,6,8,3,9],[8,3,8,5,1],[7,6,5,4,3]]
+det(matriz1)
