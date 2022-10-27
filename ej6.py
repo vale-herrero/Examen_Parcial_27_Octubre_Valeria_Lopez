ba1cc8aa5a14025f88717ccb96e5756e93eb6f3f
vale-herrero <vale4134@gmail.com>
2022-10-27 20:10:39 +0200

    hecho ej6

--- ej6.py
+++ ej6.py
@@ -1,1 +1,10 @@
 
+import numpy as np
+
+
+def determinante(matriz):
+    determinante = np.linalg.det(matriz)
+    return determinante
+
+matriz = np.array([[3,5,6,7,8],[1,2,3,4,5],[3,6,8,3,9],[8,3,8,5,1],[7,6,5,4,3]])
+print(determinante(matriz))
